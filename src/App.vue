d16366568700a80418784e7ad9c8030395d264fd
javawebuser <3215712411@qq.com>
2020-11-08 14:46:43 +0800

    vuex

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <add></add>
+    <p>--------------------------------</p>
+    <jian></jian>
+  </div>
+</template>
+
+<script>
+import add from './components/add'
+import jian from './components/jian'
+
+export default {
+  data () {
+    return {}
+  },
+  components: {
+    add,
+    jian
+  }
+}
+</script>
---
+++ src/components/add.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>count:</h3>
+    <button>+1</button>
+  </div>
+</template>
+
+<script> 
+export default {
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
